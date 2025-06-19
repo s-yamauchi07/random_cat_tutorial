4820613d1aece7e34a0a32b5bfbc89907a7cd8dc
s-yamauchi07 <hand1nhand306@gmail.com>
2025-06-19 22:44:02 +0900

    styleの適用

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -1,13 +1,30 @@
-// コンポーネントの引数を定義する
+"use client";
+import { useState } from "react";
+import { fetchImage } from "./fetch-image";
+import styles from "../page.module.css";
+
 type CatImageProps = {
   url: string;
 };
  
 // 画像を表示するコンポーネント
 export function CatImage({ url }: CatImageProps) {
+  const [imageUrl, setImageUrl] = useState(url);
+
+  const refreshImage = async () => {
+    setImageUrl(""); // 画像の初期化
+    const image = await fetchImage();
+    setImageUrl(image.url); // 新しい画像のURLを設定
+  }
+
   return (
-    <div>
-      <img src={url} />
+    <div className={styles.page}>
+      <button onClick={refreshImage} className={styles.button}>
+        他のニャンコも見る
+      </button>
+      <div className={styles.frame}>
+        {imageUrl && <img src={imageUrl} className={styles.img}/>}
+      </div>
     </div>
   );
 }
--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -1,11 +1,16 @@
+"use server";
+import { CAT_API_KEY } from "@/env";
+
 type Image = {
   url: string;
 }
 
-
 export async function fetchImage(): Promise<Image> {
-  const res: Response = await fetch("https://api.thecatapi.com/v1/images/search");
+  const res: Response = await fetch("https://api.thecatapi.com/v1/images/search",{
+    headers: {
+      "x-api-key": CAT_API_KEY,
+    }
+  });
   const images = await res.json();
-  
   return images[0];
 }
